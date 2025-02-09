daeb7995caa4acd5e3103b7869100018548a23ba
TioHades <rochacaik@gmail.com>
2025-02-09 20:14:22 -0300

    footer update

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import royal from "../../../public/royal.png"
+import golden from "../../../public/golden.png"
+import primier from "../../../public/primier.png"
+import natural from "../../../public/natural.png"
+import whiskas from "../../../public/whiskas.png"
+
+
+
+const brands =[
+    {name: "Royal Canin", logo: royal},
+    {name: "Golden", logo: golden},
+    {name: "Primier", logo: primier},
+    {name: "Formula Natural", logo: natural},
+    {name: "Whiskas", logo: whiskas},
+    {name: "Golden", logo: golden}
+]
+
+export function Footer() {
+    return(
+        <section className="bg-[#E84c3d] py-16 text-white">
+            
+        </section>
+    )
+}
