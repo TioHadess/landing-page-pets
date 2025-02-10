c547935b446dd7ed088172948d050088b54ffa14
TioHades <rochacaik@gmail.com>
2025-02-10 13:44:12 -0300

    Footer update 1.1

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,6 +4,7 @@ import golden from "../../../public/golden.png"
 import primier from "../../../public/primier.png"
 import natural from "../../../public/natural.png"
 import whiskas from "../../../public/whiskas.png"
+import { FacebookLogo, InstagramLogo, XLogo } from "@phosphor-icons/react/dist/ssr"
 
 
 
@@ -19,7 +20,78 @@ const brands =[
 export function Footer() {
     return(
         <section className="bg-[#E84c3d] py-16 text-white">
-            
+            <div className="container mx-auto px-4">
+                
+                <div className="border-b border-white/20 pb-8">
+                    <h4 className="text-3xl font-semibold mb-8 text-center">Marcas que trabalhamos</h4>
+
+                    <div className="grid grid-cols-2 lg:grid-cols-6 gap-8">
+
+                        {brands.map((item, index) =>(
+                            <div key={index} className="bg-white p-4 rounded-lg flex items-center justify-center">
+                                <Image
+                                    src={item.logo}
+                                    alt="item.name"
+                                    width={100}
+                                    height={50}
+                                    style={{
+                                        width:"auto",
+                                        height:"autor"
+                                    }}
+                                    className="object-contain"
+                                />
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
+
+                <footer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5">
+                    <div>
+                        <h3 className="text-2xl font-semibold mb-2">Pet shop Dev</h3>
+                        <p className="mb-4">Cuidando do seu melho amigo com amor e dedicação.</p>
+                        <a 
+                        href="#"
+                        className="bg-green-500 px-4 py-2 rounded-md"
+                        >
+                            Contato via Whatsapp
+                        </a>
+                    </div>
+
+                    <div>
+                    <h3 className="text-2xl font-semibold mb-2">Contatos</h3>
+                        <p>Email: teste@teste.com</p>
+                        <p>Telefone: (XX) 999999999</p>
+                        <p>Rua X, centro, Salvador / BA</p>
+                    </div>
+
+                    <div>
+                    <h3 className="text-2xl font-semibold mb-2">Redes sociais</h3>
+                        <div className="flex gap-4">
+                            <a 
+                            href="#"
+                            target="_blank"
+                            >
+                               <FacebookLogo className="w-8 h-8"/> 
+                            </a>
+                            <a 
+                            href="#"
+                            target="_blank"
+                            >
+                               <InstagramLogo className="w-8 h-8"/> 
+                            </a>
+                            <a 
+                            href="#"
+                            target="_blank"
+                            >
+                               <XLogo className="w-8 h-8"/>
+                            </a>
+                        </div>
+                    </div>
+
+                </footer>
+
+            </div>
         </section>
     )
 }
